426337e18d5367885787c12a9432b477c874d3a9
lixiaopi1985 <lixiaopi@oregonstate.edu>
2019-04-19 23:48:34 -0700

    set up app for heroku

--- public/scripts/myscripts.js
+++ public/scripts/myscripts.js
@@ -28,7 +28,7 @@ weatherForm.addEventListener('submit', (e)=>{
 
     e.preventDefault();
 
-    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response)=>{
+    fetch(`/weather?address=${search.value}`).then((response)=>{
     response.json().then((data)=>{
 
         if(data.error){
